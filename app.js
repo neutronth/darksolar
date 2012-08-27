1a5cb0f6153d6b3ec7eaa1de7336b02228857736
Neutron Soutmun <neo.neutron@gmail.com>
2012-08-27 15:07:03 +0700

    Implement master-workers process with nodejs cluster module

--- app.js
+++ app.js
@@ -3,10 +3,9 @@
  * Module dependencies.
  */
 
-var express = require('express'),
-    MongoStore = require ('connect-mongo')(express), 
-    routes = require ('./routes');
-    auth = require ('./api/auth');
+
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 
 const crypto = require ('crypto');
       fs = require ('fs');
@@ -18,56 +17,93 @@ var credentials = {
   cert: certificate,
 };
 
-var app = module.exports = express.createServer(credentials);
 
-var config = require ('./settings'); 
-app.config = config;
+var workers = [];
 
+if (cluster.isMaster) {
+  // Fork workers
+  for (var i = 0; i < numCPUs; i++) {
+    var worker = cluster.fork ();
+    workers.push (worker);
+  }
+} else { /* Child process */
+  var express = require('express'),
+      MongoStore = require ('connect-mongo')(express)
+      routes = require ('./routes');
+      auth = require ('./api/auth');
 
-// Configuration
+  var app = module.exports = express.createServer(credentials);
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.cookieParser());
-  app.use(express.session({
-    secret: app.config.cookie_secret,
-    cookie: {
-      maxAge: 1800000,
-    },
-    store: new MongoStore (app.config.StoreDb), 
-  }));
-  app.use(auth.everyauth.middleware ());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+  var config = require ('./settings');
+  app.config = config;
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-
-  process.on ('uncaughtException', function (err) {
-    console.log ('Caught exception: ', err);
+  // Configuration
+  //
+  app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session({
+      secret: app.config.cookie_secret,
+      cookie: {
+        maxAge: 1800000,
+      },
+      store: new MongoStore (app.config.StoreDb),
+    }));
+    app.use(auth.everyauth.middleware ());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
   });
-});
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
+  app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
-  process.on ('uncaughtException', function (err) {
-    /* Silently ignore */
+    process.on ('uncaughtException', function (err) {
+      console.log ('Caught exception: ', err);
+    });
   });
+
+  app.configure('production', function(){
+    app.use(express.errorHandler());
+
+    process.on ('uncaughtException', function (err) {
+      /* Silently ignore */
+    });
+  });
+
+  auth.everyauth.helpExpress (app, { userAlias: '__user__' });
+  auth.everyauth.password
+    .loginLocals ({ title: 'Login: ' + app.config.appName })
+    .registerLocals ({ title: 'Register: ' + app.config.appName });
+
+  // Routes
+  routes.init (app);
+
+  app.listen(3000, function(){
+    console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  });
+}
+
+cluster.on ('death', function (worker) {
+  console.log ('Worker ' + worker.pid + ' died, respawn');
+
+  for (var i = 0; i < workers.length; i++) {
+    var death = workers[i];
+
+    if (worker.pid === death.pid)
+      workers.splice (i);
+  }
+
+  var respawn = cluster.fork ();
+  workers.push (respawn);
 });
 
-auth.everyauth.helpExpress (app, { userAlias: '__user__' });
-auth.everyauth.password
-  .loginLocals ({ title: 'Login: ' + app.config.appName })
-  .registerLocals ({ title: 'Register: ' + app.config.appName });
+process.on ('SIGTERM', function () {
+  for (var i = 0; i < workers.length; i++) {
+    workers[i].kill ('SIGTERM');
+  }
 
-// Routes
-routes.init (app);
-
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  process.exit (1);
 });
