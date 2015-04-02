c2d5c0d8dd7c702b080ac12ea276b0953b737ce7
Neutron Soutmun <neo.neutron@gmail.com>
2015-04-02 10:21:06 +0700

    Add page header and breadcrumb

--- public/js/views/menu.js
+++ public/js/views/menu.js
@@ -60,9 +60,14 @@ window.MenuView = Backbone.View.extend({
 
     this.$el.i18n ();
 
+    this.updatePageHeader (this.readCookie ("current-path"));
+
     if ($.AdminLTE != undefined) {
       $.AdminLTE.tree ('.sidebar');
     }
+
+    $(".menuitem", this.$el)
+      .click ($.proxy (this.clickInvoked, this));
   },
 
   add: function (path, link, icon) {
@@ -90,6 +95,86 @@ window.MenuView = Backbone.View.extend({
     }
 
     this.render ();
+  },
+
+  clickInvoked: function (event) {
+    var link = $(event.target);
+    while (!link.hasClass ('menuitem'))
+      link = link.parent ();
+
+    var path = link.attr ("data-path");
+    this.createCookie ("current-path", path, "");
+    this.updatePageHeader (path);
+  },
+
+  updatePageHeader: function (path) {
+    if (!path)
+      return;
+
+    var page_header = $("#page_header");
+    var label = "<span data-i18n='nav:" + path + "'></span>";
+
+    page_header.html (label);
+    page_header.i18n ();
+
+    this.updateBreadCrumb (path);
+  },
+
+  updateBreadCrumb: function (path) {
+    if (!path)
+      return;
+
+    var path_split = path.split ('/');
+    var breadcrumb_html = "";
+    var cur_path = "";
+
+    for (var i = 0; i < path_split.length; i++) {
+      cur_path += path_split[i];
+
+      var cur_icon = "fa fa-folder-o";
+      if (this.iconList.hasOwnProperty (cur_path)) {
+        cur_icon = this.iconList[cur_path];
+      }
+
+      var icon_html = "<span class='" + cur_icon + "' style='margin-right: 5px;'></span> ";
+      var label_html = "<span data-i18n='nav:" + cur_path + "'></span>";
+
+      breadcrumb_html += "<li>" + icon_html + label_html + "</li>";
+      cur_path += "/";
+    }
+
+    var breadcrumb = $("#page_breadcrumb")
+    breadcrumb.html (breadcrumb_html);
+    breadcrumb.i18n ();
+  },
+
+  createCookie: function (name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date ();
+      date.setTime (date.getTime () + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toGMTString ();
+    }
+
+    document.cookie = name + "=" + value + expires + "; path=/";
+  },
+
+  readCookie : function (name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt (0) == ' ') c = c.substring (1, c.length);
+
+      if (c.indexOf (nameEQ) == 0)
+        return c.substring (nameEQ.length, c.length);
+    }
+
+    return null;
+  },
+
+  eraseCookie: function (name) {
+    this.createCookie (name, "", -1);
   }
 });
 
@@ -131,7 +216,8 @@ window.MenuViewMenuItem = Backbone.View.extend({
   render: function () {
     var icon  = "<span class='" + this.icon + "'></span> ";
     var label = "<span data-i18n='nav:" + this.path + "'>" + this.label + "</span>";
-    this.$el.append ("<a href='" + this.link + "'>" + icon + label + "</a>");
+    this.$el.append ("<a href='" + this.link + "' class='menuitem' data-path='" + this.path + "'>" + icon + label + "</a>");
+
     return this;
   },
 });
